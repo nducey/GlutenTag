82bf7e9271bf4788e9c22d6e23e1b2f2be27ca0e
BALENCIAGA! <33734343+nducey@users.noreply.github.com>
2019-11-22 15:46:19 -0800

    Initial Commette
    Initial filum

---
+++ R/FunctionForPackage.R
@@ -0,0 +1,7 @@
+Let <- function(){
+  letrets <- c("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
+  i <- runif(1,1,26)
+  l <- letrets[i]
+  return(l)
+}
+
